c83be1fcfd8d595b8b943278046fd75d11eb057a
Sam Thabet <sallam.thabet@gmail.com>
2021-07-31 17:11:54 -0400

    Added Jquery, fetch of api, function of retrieving info btn

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 let search = document.getElementsByClassName("space");
-let searchBtn = document.getElementById("button");
-const apiKey = "50c455b8512bb93ad4b4a31734d019c9";
+var searchBtn = document.getElementById("btn");
+const apiKey = "94f1070b60b1605c526e15498b20130c";
+
+document.querySelector("#btn").addEventListener("click", function () {
+  let searchTerm = document.querySelector("#city").value;
+  var id = "city";
+  localStorage.setItem(city, searchTerm);
+  console.log(searchTerm);
+
+  fetch(
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+      searchTerm +
+      "&APPID=" +
+      apiKey
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+});
+
+//Fetch Function
